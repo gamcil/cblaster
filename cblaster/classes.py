feeb82dda09d056d59d426e308fe09c7cea9047a
Cameron Gilchrist <clmgilchrist@gmail.com>
2020-02-20 14:05:53 +0800

    add Session class, refactor report formatting to it

--- cblaster/classes.py
+++ cblaster/classes.py
@@ -5,6 +5,7 @@ This module stores the classes (Organism, Scaffold, Hit) used in cblaster.
 """
 
 import re
+import json
 
 
 def generate_header_string(text, symbol="-"):
@@ -59,13 +60,166 @@ def generate_cluster_table(hits, decimals=4, show_headers=True, human=True):
     return "\n".join(",".join(hit) for hit in rows)
 
 
-class Organism:
+class Serializer:
+    """JSON serialiser"""
+
+    def to_dict(self):
+        raise NotImplementedError
+
+    @classmethod
+    def from_dict(self, d):
+        raise NotImplementedError
+
+    def to_json(self, fp=None, **kwargs):
+        d = self.to_dict()
+        if fp:
+            json.dump(d, fp, **kwargs)
+        else:
+            return json.dumps(d, **kwargs)
+
+    @classmethod
+    def from_json(cls, js):
+        if isinstance(js, str):
+            d = json.loads(js)
+        else:
+            d = json.load(js)
+        return cls.from_dict(d)
+
+
+class Session(Serializer):
+    """A search session."""
+
+    def __init__(self, queries, params, organisms=None):
+        self.queries = queries
+        self.params = params
+        self.organisms = organisms if organisms else []
+
+    def to_dict(self):
+        return {
+            "queries": self.queries,
+            "params": self.params,
+            "organisms": [o.to_dict() for o in self.organisms],
+        }
+
+    @classmethod
+    def from_dict(cls, d):
+        return cls(
+            d["queries"],
+            d["params"],
+            organisms=[Organism.from_dict(o) for o in d["organisms"]],
+        )
+
+    def count_queries(self, hits, identity=False):
+        """Count number of hits for each query in a collection of Hits.
+        If identity=True, will return the maximum identity hit for each query.
+        """
+
+        def count(query):
+            if identity:
+                return max([h.identity if query == h.query else 0 for h in hits])
+            return sum(query == h.query for h in hits)
+
+        return [count(query) for query in self.queries]
+
+    def form_matrices(self):
+        """Form 2D count matrices for plotting."""
+
+        def form_row(organism, accession, cluster):
+            genus, species = organism.name.split(" ", 1)
+            name = f"$\it{{{genus[0]}. {species}}}$ {organism.strain}"
+            scaf = f"{accession}:{cluster[0].start}-{cluster[-1].end}"
+            cnts = self.count_queries(cluster)
+            idts = self.count_queries(cluster, identity=True)
+            return name, scaf, cnts, idts
+
+        names = []  # Species names for dendrogram yticklabels
+        scafs = []  # Scaffold positions for matshow yticklabels
+        counts = []  # Total # of hits in cluster per query
+        idents = []  # Maximum identity of single hit per query
+
+        for organism in self.organisms:
+            for accession, scaffold in organism.scaffolds.items():
+                for cluster in scaffold.clusters:
+                    name, scaf, cnts, idts = form_row(organism, accession, cluster)
+                    names.append(name)
+                    scafs.append(scaf)
+                    counts.append(cnts)
+                    idents.append(idts)
+
+        return names, scafs, counts, idents
+
+    def _summary(self, human=True, headers=True):
+        """Generate summary of >1 Organisms, print to console or write to file.
+
+        Parameters
+        ----------
+        organisms: list
+        output: open file handle
+        """
+        return "\n\n\n".join(
+            organism.summary(headers=headers, human=human)
+            for organism in self.organisms
+            if organism.count_hit_clusters() > 0
+        )
+
+    def _binary(self, human=False, headers=True, identity=False):
+        """Generate a binary summary table.
+
+        For example:
+
+        Organism  Scaffold  Start  End    Query1  Query2  Query3  Query4
+        Org 1     Scaf_1    1      20000  2       1       1       1
+        Org 1     Scaf_3    3123   40302  0       1       0       1
+        """
+
+        columns = len(self.queries) + 4
+
+        rows = [
+            [
+                organism.full_name,
+                accession,
+                str(cluster[0].start),
+                str(cluster[-1].end),
+                [str(x) for x in self.count_queries(cluster, identity=identity)],
+            ]
+            for organism in self.organisms
+            for accession, scaffold in organism.scaffolds.items()
+            for cluster in scaffold.clusters
+        ]
+
+        if headers:
+            rows.insert(0, ["Organism", "Scaffold", "Start", "End", *self.queries])
+
+        if human:
+            # Calculate lengths of each column for spacing
+            lengths = [max(len(row[i]) for row in rows) for i in range(columns)]
+            table = "\n".join(
+                "  ".join(f"{row[i]:{lengths[i]}}" for i in range(columns))
+                for row in rows
+            )
+        else:
+            table = "\n".join(",".join(row) for row in rows)
+
+        return table
+
+    def format(self, form, fp, human=True, headers=True, **kwargs):
+        """Tables, etc"""
+        if form == "summary":
+            table = self._summary(human=human, headers=headers, **kwargs)
+        elif form == "binary":
+            table = self._binary(human=human, headers=headers, **kwargs)
+        else:
+            raise ValueError("Expected 'summary' or 'binary'")
+        print(table, file=fp)
+
+
+class Organism(Serializer):
     """The Organism class stores hits on scaffolds"""
 
-    def __init__(self, name, strain):
+    def __init__(self, name, strain, scaffolds=None):
         self.name = name
         self.strain = strain
-        self.scaffolds = {}
+        self.scaffolds = scaffolds if scaffolds else {}
 
     def __str__(self):
         total_scaffolds = len(self.scaffolds)
@@ -102,16 +256,32 @@ class Organism:
             return f"{self.name}"
         return f"{self.name} {self.strain}"
 
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "strain": self.strain,
+            "scaffolds": [scaffold.to_dict() for scaffold in self.scaffolds.values()],
+        }
 
-class Scaffold:
-    """The Scaffold object stores Hit objects, as well as the logic for detecting Hit
-    clusters.
-    """
+    @classmethod
+    def from_dict(cls, d):
+        return cls(
+            d["name"],
+            d["strain"],
+            scaffolds={
+                scaffold["accession"]: Scaffold.from_dict(scaffold)
+                for scaffold in d["scaffolds"]
+            },
+        )
 
-    def __init__(self, accession, hits=None):
+
+class Scaffold(Serializer):
+    """A genomic scaffold containing BLAST hits."""
+
+    def __init__(self, accession, clusters=None, hits=None):
         self.accession = accession
         self.hits = hits if hits else []
-        self.clusters = []
+        self.clusters = clusters if clusters else []
 
     def __str__(self):
         return "SCAFFOLD: {} [{} hits in {} clusters]".format(
@@ -136,11 +306,28 @@ class Scaffold:
 
         return report
 
+    def to_dict(self):
+        return {
+            "accession": self.accession,
+            "hits": [hit.to_dict() for hit in self.hits],
+            "clusters": [
+                [self.hits.index(hit) for hit in cluster] for cluster in self.clusters
+            ],
+        }
 
-class Hit:
-    """The Hit class stores BLAST hits and their genomic contexts.
+    @classmethod
+    def from_dict(cls, d):
+        hits = [Hit.from_dict(hit) for hit in d["hits"]]
+        clusters = [[hits[ix] for ix in cluster] for cluster in d["clusters"]]
+        return cls(accession=d["accession"], hits=hits, clusters=clusters)
 
-    They are first instantiated when parsing BLAST results, and are then updated with
+
+class Hit(Serializer):
+    """A BLAST hit.
+
+    Stores hit scores and genomic context.
+
+    First instantiated when parsing BLAST results, then updated with
     genomic information after NCBI is queried for IPG.
     """
 
@@ -163,8 +350,8 @@ class Hit:
 
         self.subject = subject
         self.bitscore = float(bitscore)
-        self.identity = float(identity) / 100
-        self.coverage = float(coverage) / 100
+        self.identity = float(identity)
+        self.coverage = float(coverage)
         self.evalue = float(evalue)
 
         self.start = int(start) if start is not None else None
@@ -177,6 +364,13 @@ class Hit:
             f"{self.start}-{self.end}[{self.strand}] "
             f" {self.identity:.2%}/{self.coverage:.2%}"
         )
+
+    def copy(self, **kwargs):
+        """Return a copy of this Hit object with any additional args."""
+        copy = Hit(**self.__dict__)
+        for key, val in kwargs.items():
+            setattr(copy, key, val)
+        return copy
 
     def values(self, decimals=4):
         """Format all attributes of this hit for printing.
@@ -205,3 +399,20 @@ class Hit:
             str(self.end),
             self.strand,
         ]
+
+    def to_dict(self):
+        return {
+            "query": self.query,
+            "subject": self.subject,
+            "identity": self.identity,
+            "coverage": self.coverage,
+            "evalue": self.evalue,
+            "bitscore": self.bitscore,
+            "start": self.start,
+            "end": self.end,
+            "strand": self.strand,
+        }
+
+    @classmethod
+    def from_dict(cls, d):
+        return cls(**d)
