2d7e29edb5273ff832511fb460fb91eaafc4abe4
Bram <eckelradekip10@gmail.com>
2020-11-27 12:03:00 +0100

    added plot_clusters subcommand to main and created plot_clusters module

---
+++ cblaster/plot_clusters.py
@@ -0,0 +1,20 @@
+
+import tempfile
+from cblaster.extract_clusters import extract_clusters
+
+
+def plot_clusters(
+    session=None,
+    files=None,
+    cluster_numbers=None,
+    score_threshold=None,
+    organisms=None,
+    scaffolds=None,
+    allign_clusters=False,
+    identity=0.3,
+    plot_outfile=None,
+    allignment_out=None,
+    cluster_out=None,
+    prefix="",
+):
+    pass
