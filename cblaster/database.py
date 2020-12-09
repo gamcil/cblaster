c57f35d632551a58884ef7e9c71b67923efcdd53
LianaGrieken <lianavangrieken@gmail.com>
2020-12-09 12:15:44 +0100

    added file extension to genbank parser

--- cblaster/database.py
+++ cblaster/database.py
@@ -66,7 +66,6 @@ def sqlite_to_fasta(path, database):
         for (record,) in cur.execute(FASTA):
             fasta.write(record)
 
-
 def query_database(ids, database):
     """Queries the cblaster SQLite3 database for a collection of gene IDs.
 
@@ -82,6 +81,76 @@ def query_database(ids, database):
         cur = con.cursor()
         return cur.execute(query, ids).fetchall()
 
+    def __init__(self, organisms=None):
+        self.organisms = organisms if organisms else []
+
+    def __iter__(self):
+        return iter(self.organisms)
+
+    def write_fasta(self, handle):
+        """Formats organisms in the database to indexed FASTA format.
+        Builds FASTA of each organism, then writes to given handle.
+        """
+        for i, organism in enumerate(self.organisms):
+            fasta = ""
+            for j, scaffold in enumerate(organism.scaffolds):
+                for k, feature in enumerate(scaffold.features):
+                    try:
+                        sequence = feature.qualifiers["translation"]
+                    except KeyError:
+                        continue
+                    fasta += f">{i}_{j}_{k}\n{sequence}\n"
+            handle.write(fasta)
+
+    @classmethod
+    def from_files(cls, files):
+        """Builds a new Database from a collection of GenBank files.
+
+        For example:
+
+        >>> db = Database.from_files(['path/to/file.gbk', 'path/to/file.gbk'])
+
+        Only CDS features are parsed.
+        """
+        organisms = []
+        LOG.info("Parsing %i files...", len(files))
+        for index, file in enumerate(files, 1):
+            with open(file) as handle:
+                LOG.info("%i. %s", index, file)
+                if any(key in handle.name for key in ["gb", "gbk", "genbank", "gp"]):
+                    organism = genbank.parse(handle, feature_types=["CDS"])
+                elif any(key in handle.name for key in ["gff", "gff3"]):
+                    organism = parse_gff(handle)
+                else:
+                    LOG.warning(
+                        "Expected GenBank (.gb, .gbk or .genbank) or"
+                        " GFF3 (.gff, .gff3) file extensions. Skipping..."
+                    )
+                organisms.append(organism)
+        return cls(organisms)
+
+    def to_list(self):
+        """Serialises all organisms in this database to dict."""
+        return [organism.to_dict() for organism in self]
+
+    def to_json(self, handle, indent=None):
+        """Writes database to file in JSON format."""
+        json.dump(self.to_list(), handle, indent=indent)
+
+    @classmethod
+    def from_json(cls, json_file):
+        """Load a Database from JSON."""
+        with open(json_file) as handle:
+            js = json.load(handle)
+        organisms = [g2j.classes.Organism.from_dict(d) for d in js]
+        return cls(organisms)
+
+    def makedb(self, name):
+        """Convenience function to write FASTA and generate diamond DB"""
+        fasta = f"{name}.faa"
+        with open(fasta, "w") as handle:
+            self.write_fasta(handle)
+        diamond_makedb(fasta, name)
 
 def diamond_makedb(fasta, name):
     """Builds a DIAMOND database from JSON.
