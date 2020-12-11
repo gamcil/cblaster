74238af25056e2f1f4403216f1cb1b54542e079d
Bram <eckelradekip10@gmail.com>
2020-12-11 10:38:24 +0100

    addition of some documentation

--- cblaster/plot_clusters.py
+++ cblaster/plot_clusters.py
@@ -22,18 +22,6 @@ from cblaster import embl
 LOG = logging.getLogger(__name__)
 
 
-def find_genbank_files(files):
-    genbank_files = []
-    for path in files:
-        path_obj = Path(path)
-        if path_obj.is_dir():
-            genbank_files.extend(
-                [str(po.resolve()) for po in path_obj.iterdir() if po.suffix in (".gbk", ".gb", ".genbank", ".gbff")])
-        elif path_obj.suffix in (".gbk", ".gb", ".genbank", ".gbff"):
-            genbank_files.append(str(path_obj.resolve()))
-    return genbank_files
-
-
 def query_to_clinker_cluster(query_file):
     with open(query_file) as query:
         if any(query_file.endswith(ext) for ext in (".gbk", ".gb", ".genbank", ".gbff")):
@@ -66,13 +54,25 @@ def query_to_clinker_cluster(query_file):
     return ClinkerCluster("Query_cluster", loci)
 
 
-def clusters_to_clinker_allignments(query_cluster, both_clusters):
+def clusters_to_clinker_alignments(clinker_query_cluster, clusters):
+    """Create clinker.Alignments classes between the query cluster and all other clusters
+
+    Make clinker.Link objects between all genes of the query and the genes in the clusters that
+    where matched during blasting.
+
+    Args:
+        clinker_query_cluster(clinker.Cluster): clinker.Cluster object of the query used for the session
+        clusters(List): a list of cblaster.Cluster objects
+    Returns:
+        a list of clinker.Alignment objects
+    """
     allignments = []
-    for cblaster_cluster, clinker_cluster in both_clusters:
-        allignment = ClinkerAlignment(query=query_cluster, target=clinker_cluster)
+    for cblaster_cluster in clusters:
+        clinker_cluster = cblaster_cluster.to_clinker_cluster()
+        allignment = ClinkerAlignment(query=clinker_query_cluster, target=clinker_cluster)
         for subject in cblaster_cluster.subjects:
             best_hit = max(subject.hits, key=lambda x: x.bitscore)
-            query_gene = _gene_from_clinker_cluster(query_cluster, best_hit.query)
+            query_gene = _gene_from_clinker_cluster(clinker_query_cluster, best_hit.query)
             subject_gene = _gene_from_clinker_cluster(clinker_cluster, best_hit.subject)
             allignment.add_link(query_gene, subject_gene, best_hit.identity, 0)
         allignments.append(allignment)
@@ -80,7 +80,18 @@ def clusters_to_clinker_allignments(query_cluster, both_clusters):
 
 
 def _gene_from_clinker_cluster(cluster, gene_label):
-    """Here because of a bug in clinker where a name attribute is called when that should be label"""
+    """Get a gene from a clinker.Cluster object
+
+    works the same as clinker.Cluster.get_gene, except that this function is
+    broken at the moment since clinker.Gene objects have no name attribute
+    anymore but label objects instead.
+
+    Args:
+        cluster(clinker.Cluster): a clinker.Cluster object
+        gene_label(str): the label of the clinker.Gene object that is requested
+    Returns:
+        a clinker.Gene object or None of no sutch gene exists in the cluster.
+    """
     for locus in cluster.loci:
         for gene in locus.genes:
             if gene.label == gene_label:
@@ -88,6 +99,13 @@ def _gene_from_clinker_cluster(cluster, gene_label):
 
 
 def allignments_to_clinker_global_alligner(allignments):
+    """Create a clinker.Globalaligner object from alignments
+
+    Args:
+        allignments (List): a list of clinker.Aligner objects
+    Returns:
+        a clinker.Globalaligner object
+    """
     global_aligner = ClinkerGlobalaligner()
     for allignment in allignments:
         global_aligner.add_alignment(allignment)
@@ -95,23 +113,34 @@ def allignments_to_clinker_global_alligner(allignments):
 
 
 def plot_clusters(
-    session=None,
+    session,
     cluster_numbers=None,
     score_threshold=None,
     organisms=None,
     scaffolds=None,
     plot_outfile=None,
 ):
+    """Plot Cluster objects from a Session file
+
+    Args:
+        session (string): path to a session.json file
+        cluster_numbers (list): cluster numbers to include
+        score_threshold (float): minum score in order for a cluster to be included
+        organisms (list): Organism filtering regular expressions, clusters for
+         these organisms are included
+        scaffolds(list): clusters on these scaffolds are included
+        plot_outfile (str): path to a file for the final plot
+    """
     logging.info("Starting generation of cluster plot with clinker.")
     with open(session, "r") as f:
         session = Session.from_json(f.read())
+
+    # filter the cluster using the filter functions from extract_clusters modue
     cluster_hierarchies = extract_cluster_hierarchies(session, cluster_numbers, score_threshold, organisms, scaffolds)
-    both_clusters = []
-    for cluster, scaffold_acs, org_name in cluster_hierarchies:
-        both_clusters.append((cluster, cluster.to_clinker_cluster()))
+
     clinker_query_cluster = query_to_clinker_cluster(session.params["query_file"])
 
-    allignments = clusters_to_clinker_allignments(clinker_query_cluster, both_clusters)
+    allignments = clusters_to_clinker_alignments(clinker_query_cluster, [h[0] for h in cluster_hierarchies])
     global_aligner = allignments_to_clinker_global_alligner(allignments)
 
     clinker_plot_clusters(global_aligner, plot_outfile, use_file_order=True)
