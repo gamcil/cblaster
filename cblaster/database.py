9f0025edc19eb05553f0fb7de0e7eb3e17eb6914
Cameron Gilchrist <clmgilchrist@gmail.com>
2020-12-14 19:02:47 +0800

    parse genomes using BioPython, add SQLite3 db init/insert/query/fasta

--- cblaster/database.py
+++ cblaster/database.py
@@ -5,124 +5,319 @@ This module handles creation of local JSON databases for non-NCBI lookups.
 import json
 import logging
 import subprocess
+import sqlite3
+import warnings
 
 from pathlib import Path
 
-import g2j
-from g2j import genbank, gff3
+import gffutils
+from gffutils import biopython_integration
+
+from Bio import SeqIO, BiopythonParserWarning
+from Bio.SeqFeature import FeatureLocation
 
 from cblaster import helpers
+
+
+# ignore malformed locus warnings
+warnings.simplefilter('ignore', BiopythonParserWarning)
+
 
 LOG = logging.getLogger("cblaster")
 
 
-def parse_gff(gff_handle, fasta_handle=None):
-    """Parses a GFF file using genome2json.
+SCHEMA = """\
+CREATE TABLE gene (
+    id              INTEGER PRIMARY KEY,
+    name 	    TEXT,
+    start_pos       INTEGER,
+    end_pos	    INTEGER,
+    strand	    INTEGER,
+    translation	    TEXT,
+    scaffold        TEXT,
+    organism        TEXT
+);\
+"""
 
-    Expects the given file to contain a ##FASTA directive, or a separate FASTA
-    file to be provided.
+QUERY = """\
+SELECT
+    name,
+    start_pos,
+    end_pos,
+    strand,
+    scaffold,
+    organism
+FROM
+    gene
+WHERE
+    id IN ({})\
+"""
+
+FASTA = 'SELECT ">"||gene.id||"\n"||gene.translation||"\n" FROM gene'
+
+INSERT = """\
+INSERT INTO gene (
+    name,
+    start_pos,
+    end_pos,
+    strand,
+    translation,
+    scaffold,
+    organism
+)
+VALUES
+    (?, ?, ?, ?, ?, ?, ?)\
+"""
+
+GET_ORGANISM_IDS = "SELECT id, name FROM organism"
+
+FASTA_SUFFIXES = (".fa", ".fsa", ".fna", ".fasta", ".faa")
+GBK_SUFFIXES = (".gbk", ".gb", ".genbank", ".gbf", ".gbff")
+GFF_SUFFIXES = (".gtf", ".gff", ".gff3")
+EMBL_SUFFIXES = (".embl",)
+
+
+def find_fasta(gff_path):
+    """Finds a FASTA file corresponding to the given GFF path."""
+    for suffix in FASTA_SUFFIXES:
+        path = Path(gff_path).with_suffix(suffix)
+        if path.exists():
+            return path
+
+
+def parse_fasta(path):
+    with open(path) as fp:
+        records = list(SeqIO.parse(fp, "fasta"))
+    return records
+
+
+def find_regions(directives):
+    """Looks for ##sequence-region directives in a list of GFF3 directives."""
+    regions = {}
+    for directive in directives:
+        if directive.startswith("sequence-region"):
+            _, accession, start, end = directive.split(" ")
+            regions[accession] = (int(start), int(end))
+    return regions
+
+
+def parse_gff(path):
+    """Parses GFF and corresponding FASTA using GFFutils.
+
+    - Parse gene/CDS features, translations & real coordinates
+    - Convert to BioPython SeqRecords/SeqFeatures
+    - All other formats just str -> SeqIO.parse(fp, 'filetype')
+      then check for 'gff'
     """
-    LOG.info(f"Parsing GFF file: {gff_handle.name}")
-    organism = gff3.parse(
-        gff_handle,
-        fasta_handle=fasta_handle,
-        feature_types=["CDS"],
-        save_scaffold_sequence=True,
+    fasta = find_fasta(path)
+    if not fasta:
+        raise FileNotFoundError(f"Could not find partner FASTA file for {path}")
+
+    # Parse FASTA and create GFFUtils database
+    fasta = parse_fasta(fasta)
+    gff = gffutils.create_db(
+        str(path),
+        ":memory:",
+        force=True,
+        merge_strategy="create_unique",
+        sort_attribute_values=True
     )
+    regions = find_regions(gff.directives)
 
-    # Collapse CDS features and generate translations
-    organism.collapse()
-    organism.translate()
-    organism.sort()
+    # Find features for each record in the FASTA file
+    for record in fasta:
+        try:
+            record_start, _ = regions[record.id]
+        except KeyError:
+            record_start = 1
 
-    # Remove scaffold sequences after CDS features are translated
-    for scaffold in organism.scaffolds:
-        scaffold.sequence = ""
+        # Normalise Feature location based on ##sequence-region directive.
+        # Necessary for extracted GFF3 files that still store coordinates
+        # relative to the entire region. If no sequence-region directive
+        # is found, assumes 1 (i.e. default sequence start).
+        cds_features = []
+        for feature in gff.region(seqid=record.id, featuretype=["gene", "CDS"]):
+            feature = biopython_integration.to_seqfeature(feature)
+            feature.location = FeatureLocation(
+                feature.location.start - record_start - 1,
+                feature.location.end - record_start,
+                strand=feature.location.strand
+            )
+            if feature.type == "CDS":
+                cds_features.append(feature)
+            else:
+                record.features.append(feature)
 
-    # Set the name of the organism from the ##species directive
-    # TODO: NCBI taxonomy lookups when ##species https://...
-    organism.name = organism.qualifiers.get("species", None)
+        if not record.features:
+            raise ValueError(f"Found no CDS features in {record.id} [{path}]")
 
-    return organism
+        # Merge CDS features into singular SeqFeature objects, add them to record
+        previous = None
+        for feature in sorted(cds_features, key=lambda f: f.location.start):
+            seqid = feature.qualifiers["ID"][0]
+            same_feature = previous == seqid
+            if not previous:
+                previous = seqid
+            if same_feature:
+                record.features[-1].location += feature.location
+            else:
+                record.features.append(feature)
+                previous = seqid
+
+        # Sort, then generate insertion tuples like with other formats
+        record.features.sort(key=lambda f: f.location.start)
+
+    return fasta
 
 
-class Database:
-    """A cblaster database.
+def parse_file(path):
+    """Dispatches a given file path to the correct parser given its extension.
 
-    This class acts as a wrapper around the genome2json classes (organism, scaffold,
-    protein), consolidating them for use as a database.
+    Args:
+        path (str): Path to genome file
+    Returns:
+        SeqRecord
     """
+    suffix = Path(path).suffix.lower()
+    if suffix in GBK_SUFFIXES:
+        file_type = "genbank"
+    elif suffix in EMBL_SUFFIXES:
+        file_type = "embl"
+    elif suffix in GFF_SUFFIXES:
+        return parse_gff(path)
+    else:
+        raise ValueError(f"File {path} has invalid extension ({suffix})")
+    with open(path) as fp:
+        return list(SeqIO.parse(fp, file_type))
 
-    def __init__(self, organisms=None):
-        self.organisms = organisms if organisms else []
 
-    def __iter__(self):
-        return iter(self.organisms)
+def parse_files(paths):
+    """Parses a collection of file paths and generates a dictionary of SeqRecords."""
+    organisms = {}
+    for path in paths:
+        path = Path(path)
+        if not path.exists():
+            raise FileNotFoundError(f"No file exists at {path}")
+        name = path.with_suffix("").name
+        LOG.info("Parsing %s", name)
+        organisms[name] = parse_file(path)
+    return organisms
 
-    def write_fasta(self, handle):
-        """Formats organisms in the database to indexed FASTA format.
-        Builds FASTA of each organism, then writes to given handle.
-        """
-        for i, organism in enumerate(self.organisms):
-            fasta = ""
-            for j, scaffold in enumerate(organism.scaffolds):
-                for k, feature in enumerate(scaffold.features):
-                    try:
-                        sequence = feature.qualifiers["translation"]
-                    except KeyError:
-                        continue
-                    fasta += f">{i}_{j}_{k}\n{sequence}\n"
-            handle.write(fasta)
 
-    @classmethod
-    def from_files(cls, files):
-        """Builds a new Database from a collection of GenBank files.
+def init_sqlite_db(path, force=False):
+    """Initialises a cblaster SQLite database file at a given path.
 
-        For example:
+    Args:
+        path (str): Path to write SQLite3 database
+        force (bool): Overwrite pre-existing files at `path`
+    """
+    if Path(path).exists():
+        if force:
+            LOG.info("Overwriting pre-existing file at %s", path)
+            Path(path).unlink()
+        else:
+            raise FileExistsError(f"File {path} already exists but force=False")
+    else:
+        LOG.info("Initialising cblaster SQLite3 database to %s", path)
+    with sqlite3.connect(path) as con:
+        con.executescript(SCHEMA)
 
-        >>> db = Database.from_files(['path/to/file.gbk', 'path/to/file.gbk'])
 
-        Only CDS features are parsed.
-        """
-        organisms = []
-        LOG.info("Parsing %i files...", len(files))
-        for index, file in enumerate(files, 1):
-            with open(file) as handle:
-                LOG.info("%i. %s", index, file)
-                if any(key in handle.name for key in ["gb", "gbk", "genbank"]):
-                    organism = genbank.parse(handle, feature_types=["CDS"])
-                elif any(key in handle.name for key in ["gff", "gff3"]):
-                    organism = parse_gff(handle)
-                else:
-                    LOG.warning(
-                        "Expected GenBank (.gb, .gbk or .genbank) or"
-                        " GFF3 (.gff, .gff3) file extensions. Skipping..."
-                    )
-                organisms.append(organism)
-        return cls(organisms)
+def find_overlapping_location(feature, locations):
+    """Finds the index of a gene location containing `feature`."""
+    for index, (start, end) in enumerate(locations):
+        if feature.location.start >= start and feature.location.end <= end:
+            return index
 
-    def to_list(self):
-        """Serialises all organisms in this database to dict."""
-        return [organism.to_dict() for organism in self]
 
-    def to_json(self, handle, indent=None):
-        """Writes database to file in JSON format."""
-        json.dump(self.to_list(), handle, indent=indent)
+def find_gene_name(qualifiers):
+    """Finds a gene name in a dictionary of feature qualifiers."""
+    for tag in ["locus_tag", "protein_id", "id", "gene", "name", "label"]:
+        if tag in qualifiers:
+            return qualifiers[tag]
+    return "N.A."
 
-    @classmethod
-    def from_json(cls, json_file):
-        """Load a Database from JSON."""
-        with open(json_file) as handle:
-            js = json.load(handle)
-        organisms = [g2j.classes.Organism.from_dict(d) for d in js]
-        return cls(organisms)
 
-    def makedb(self, name):
-        """Convenience function to write FASTA and generate diamond DB"""
-        fasta = f"{name}.faa"
-        with open(fasta, "w") as handle:
-            self.write_fasta(handle)
-        diamond_makedb(fasta, name)
+def seqrecord_to_genes(record, source):
+    """Generates insertion tuples for genes in a SeqRecord object.
+    """
+    features = [f for f in record.features if f.type == "CDS"]
+    locations = [(f.location.start, f.location.end) for f in record.features if f.type == "gene"]
+    genes = []
+    for feature in features:
+        qualifiers = {
+            k: v[0] if isinstance(v, list) else v
+            for k, v in feature.qualifiers.items()
+        }
+        name = find_gene_name(qualifiers)
+        if "pseudo" in qualifiers:
+            LOG.warning("%s is pseudogene, skipping", name)
+            continue
+        match = find_overlapping_location(feature, locations)
+        if match:
+            start, end = locations.pop(match)
+        else:
+            start, end = feature.location.start, feature.location.end
+
+        # TODO: fix broken translations for GFF features
+        translation = (
+            qualifiers.pop("translation", None)
+            or feature.extract(record.seq).translate()
+        )
+
+        if not translation:
+            LOG.warning("Failed to find translation for %s, skipping", name)
+            continue
+        gene = (
+            str(name),
+            int(start),
+            int(end),
+            int(feature.location.strand),
+            str(translation),
+            str(record.id),  # scaffold accession
+            str(source)  # organism name from source file name
+        )
+        genes.append(gene)
+    return genes
+
+
+def organisms_to_tuples(organisms):
+    """Generates insertion tuples from a dictionary of parsed organisms."""
+    tuples = []
+    for organism, records in organisms.items():
+        for record in records:
+            genes = seqrecord_to_genes(record, organism)
+            tuples.extend(genes)
+    return tuples
+
+
+def seqrecords_to_sqlite(tuples, database):
+    """Writes a collection of SeqRecord objects to a cblaster SQLite database.
+    """
+    try:
+        with sqlite3.connect(database) as con:
+            cur = con.cursor()
+            cur.executemany(INSERT, tuples)
+    except sqlite3.IntegrityError:
+        LOG.exception("Failed to insert %i records", len(tuples))
+
+
+def sqlite_to_fasta(path, database):
+    """Writes all proteins in `database` to `path` in FASTA format."""
+    LOG.info("Dumping %s to FASTA: %s", database, path)
+    with sqlite3.connect(database) as con, open(path, "w") as fasta:
+        cur = con.cursor()
+        for (record,) in cur.execute(FASTA):
+            fasta.write(record)
+
+
+def query_database(ids, database):
+    """Queries the cblaster SQLite3 database for a collection of gene IDs."""
+    marks = ", ".join("?" for _ in ids)
+    query = QUERY.format(marks)
+    with sqlite3.connect(database) as con:
+        cur = con.cursor()
+        return cur.execute(query, ids).fetchall()
 
 
 def diamond_makedb(fasta, name):
@@ -150,3 +345,27 @@ def get_genbank_paths(folder):
         for file
         in Path(folder).iterdir() if str(file).endswith(valid_extensions)
     ]
+
+
+def makedb(paths, database, force=False):
+    """Makedb module entry point"""
+
+    sqlite_path = f"{database}.sqlite3"
+    fasta_path = f"{database}.fasta"
+    dmnd_path = f"{database}.dmnd"
+
+    LOG.info("Initialising SQLite3 database at %s", sqlite_path)
+    init_sqlite_db(sqlite_path, force=force)
+
+    LOG.info("Parsing genome files")
+    organisms = parse_files(paths)
+    tuples = organisms_to_tuples(organisms)
+
+    LOG.info("Inserting %i genes from %i organisms", len(tuples), len(organisms))
+    seqrecords_to_sqlite(tuples, sqlite_path)
+
+    LOG.info("Writing FASTA to %s", fasta_path)
+    sqlite_to_fasta(fasta_path, sqlite_path)
+
+    LOG.info("Building DIAMOND database at %s", dmnd_path)
+    diamond_makedb(fasta_path, dmnd_path)
