da2e7187641c0ce00cbba6197de80f73bf5b2586
Cameron Gilchrist <clmgilchrist@gmail.com>
2021-02-11 13:46:39 +0800

    remove names query, add query for scaffold nucleotides

--- cblaster/sql.py
+++ cblaster/sql.py
@@ -1,17 +1,46 @@
+# Creates the feature table containing genes and scaffolds
 SCHEMA = """\
-CREATE TABLE gene (
+CREATE TABLE feature (
     id              INTEGER PRIMARY KEY,
+    feature_type    TEXT,
     name            TEXT,
     start_pos       INTEGER,
     end_pos         INTEGER,
     strand          INTEGER,
-    translation     TEXT,
+    sequence        TEXT,
     scaffold        TEXT,
     organism        TEXT
 );\
 """
 
-ID_QUERY = """\
+INSERT = """\
+INSERT INTO feature (
+    name,
+    feature_type,
+    start_pos,
+    end_pos,
+    strand,
+    sequence,
+    scaffold,
+    organism
+)
+VALUES
+    (?, ?, ?, ?, ?, ?, ?, ?)\
+"""
+
+# Base for querying scaffolds in feature table
+SCAFFOLD_QUERY = """\
+SELECT
+    substr(sequence, ?, ?)
+FROM
+    feature
+WHERE
+    feature_type = \"scaffold\"
+    AND name = ?
+    AND organism = ?\
+"""
+
+GENE_QUERY = """\
 SELECT
     id,
     name,
@@ -21,21 +50,13 @@ SELECT
     scaffold,
     organism
 FROM
-    gene
+    feature
 WHERE
-    id IN ({})\
+    feature_type = \"gene\"
+    AND id IN ({})\
 """
 
-INCLUSIVE_NAME_QUERY = """\
-SELECT
-    name,
-    translation
-FROM
-    gene
-WHERE
-    name IN ({}) \
-"""
-
+# Query genes between some range not in a list
 INTERMEDIATE_GENES_QUERY = """\
 SELECT
     start_pos,
@@ -43,23 +64,14 @@ SELECT
     name,
     strand
 FROM
-    gene
+    feature
 WHERE
-    name NOT IN ({}) AND start_pos >= ? AND end_pos <= ?\
+    feature_type = \"gene\"
+    AND name NOT IN ({})
+    AND scaffold = ?
+    AND organism = ?
+    AND start_pos >= ? AND end_pos <= ?\
 """
 
+# Write database sequences in FASTA format
 FASTA = 'SELECT ">"||gene.id||"\n"||gene.translation||"\n" FROM gene'
-
-INSERT = """\
-INSERT INTO gene (
-    name,
-    start_pos,
-    end_pos,
-    strand,
-    translation,
-    scaffold,
-    organism
-)
-VALUES
-    (?, ?, ?, ?, ?, ?, ?)\
-"""
