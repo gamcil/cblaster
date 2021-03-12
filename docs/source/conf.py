ca47b55c8a66f62fe672632fc5473b1adb7ab86f
Cameron Gilchrist <clmgilchrist@gmail.com>
2021-03-12 14:36:23 +0800

    add autosectionlabel extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,15 @@ master_doc = "index"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.coverage", "sphinx.ext.napoleon"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosectionlabel"
+]
+
+# Make references have document name prefixed, e.g. ref:`document:section`
+autosectionlabel_prefix_document = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
