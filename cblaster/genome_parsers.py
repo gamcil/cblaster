d87d5f29d2144d5a318a77429c96200a76984720
biobeni <124594966+biobeni@users.noreply.github.com>
2025-05-22 15:06:19 +0200

    Fix GenBank parsing when first gene wraps start/end and is on negative strand

--- cblaster/genome_parsers.py
+++ cblaster/genome_parsers.py
@@ -246,15 +246,15 @@ def iter_overlapping_features(features):
 
     sorted_features = sorted(features, key=lambda f: f.location.start)
     first = sorted_features.pop(0)
-    group, border = [first], first.location.parts[-1].end
+    group, border = [first], min([p.end for p in first.location.parts])
 
     for feature in sorted_features:
         if feature.location.end <= border:
             group.append(feature)
-            border = max(border, feature.location.parts[-1].end)
+            border = max(border, feature.location.end)
         else:
             yield group
-            group, border = [feature], feature.location.parts[-1].end
+            group, border = [feature], feature.location.end
     yield group
 
 
